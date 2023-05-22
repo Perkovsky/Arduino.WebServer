adabfb37b5972e99fddf9225dfb0d028f9d5c6d4
Ihor Perkovskyi <igor.v.perkovsky@gmail.com>
2023-05-22 19:56:36 -0400

    SD card Web Server prototype

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,71 @@
 #include <Arduino.h>
-#include <SdFat.h>
-#include <uRTCLib.h>
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
-#include "WiFiManager.hpp"
-#include "TelegramNotifier.hpp"
-#include "RtcDateTimeProvider.hpp"
-#include "LoggerFactory.hpp"
-#include "SettingsAccessor.hpp"
-#include "DashboardService.hpp"
-#include "WebServer.hpp"
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
 
-uRTCLib rtc(0x68);
-RtcDateTimeProvider dateTimeProvider(rtc);
-SdFat sd;
-TelegramNotifier* notifier;
-LoggerFactory* logger;
-WiFiManager* wifiManager;
-DashboardService* dashboard;
-WebServer* webServer; 
+const char* ssid = "Verizon_4CGXRN";
+const char* password = "famous9-fox-apt";
+
+AsyncWebServer server(80);
+
+void initSDCard(){
+  if(!SD.begin(4)){
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD.cardType();
+
+  if(cardType == CARD_NONE){
+    Serial.println("No SD card attached");
+    return;
+  }
+
+  Serial.print("SD Card Type: ");
+  if(cardType == CARD_MMC){
+    Serial.println("MMC");
+  } else if(cardType == CARD_SD){
+    Serial.println("SDSC");
+  } else if(cardType == CARD_SDHC){
+    Serial.println("SDHC");
+  } else {
+    Serial.println("UNKNOWN");
+  }
+  uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+  Serial.printf("SD Card Size: %lluMB\n", cardSize);
+}
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.localIP().toString());
+}
 
 void setup() {
-    Serial.begin(9600);
-    sd.begin(4, SPI_HALF_SPEED);
-    URTCLIB_WIRE.begin();
-    rtc.refresh();
+  Serial.begin(9600);
+  initWiFi();
+  initSDCard();
 
-    // settings
-    SettingsAccessor settingsAccessor(sd);
-    Settings settings = settingsAccessor.getSettings();
-    
-    // notifier
-    notifier = new TelegramNotifier(settings.telegram.botId, settings.telegram.chatId);
-    
-    // logger
-    logger = new LoggerFactory(dateTimeProvider, notifier);
-    logger->writeToSerial(settings.logLevel.serial, Serial);
-    logger->writeToSdCard(settings.logLevel.sd, sd);
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("request--->>");
+    //request->send(SD, "/index.html", "text/html");
+    //request->send(SD, "/swaggerUI.html", "text/html");
+    request->send(SD, "/logo-3f834fa8.svg", "text/html");
+  });
 
-    // WiFi
-    wifiManager = new WiFiManager(settings.wifi.ssid, settings.wifi.password, *logger);
-    wifiManager->connect();
-    delay(1000);
-    wifiManager->checkConnection();
+  server.serveStatic("/", SD, "/");
 
-    // Web Server
-    dashboard = new DashboardService(*logger);
-    dashboard->bebin();
-    webServer = new WebServer(*dashboard, *logger, sd, settings.apiKey);
-    webServer->bebin(80);
+  server.begin();
 }
 
 void loop() {
-    rtc.refresh();
-    webServer->refresh();
+  
 }
--- ui/Dashboard/vite.config.js
+++ ui/Dashboard/vite.config.js
@@ -8,6 +8,9 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    assetsDir: '',
+  },
   plugins: [
     vue({ 
       template: { transformAssetUrls }
